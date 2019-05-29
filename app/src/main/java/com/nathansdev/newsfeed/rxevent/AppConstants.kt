946002ba1e6aa0e10388f1076a44daab0a45fc84
Sabari <b.sabarinathan@tcs.com>
2019-05-29 11:53:19 +0530

    removed the public api urls note: to test use any open source api

--- app/src/main/java/com/nathansdev/newsfeed/rxevent/AppConstants.kt
+++ app/src/main/java/com/nathansdev/newsfeed/rxevent/AppConstants.kt
@@ -6,6 +6,6 @@ package com.nathansdev.newsfeed.rxevent
 object AppConstants {
     const val FRAG_TAG_FEEDS_LIST = "fragmentTagFeedsList"
     const val FRAG_TAG_FEEDS_DETAIL_VIEW = "fragmentTagFeedsDetailView"
-    const val BASE_URL = "http://feeds.news24.com"
-    const val API_NEWS_XML = "/articles/Fin24/Tech/rss"
+    const val BASE_URL = "use your base url"
+    const val API_NEWS_XML = "any api"
 }
